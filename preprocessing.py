3a00385c517c534e49ede876e8b3cf9b86436822
ChristianPala <78877083+ChristianPala@users.noreply.github.com>
2022-09-23 19:24:07 +0200

    Excel into 2 csv files.

--- preprocessing.py
+++ preprocessing.py
@@ -6,8 +6,8 @@ from pathlib import Path
 
 if __name__ == '__main__':
     # data filepath
-    data_path = Path('data/online_sales_dataset.xlsx')
+    data_path = Path('data/online_sales_2009_2010_dataset.csv')
     # Load data
-    data = pd.read_csv(data_path, sep = ';')
+    data = pd.read_csv(data_path, sep=';')
     # First look
     print(data.head())

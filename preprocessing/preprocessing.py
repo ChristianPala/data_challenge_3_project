682295a91160abc4dc32efee8117508c1abdc3f7
ChristianPala <78877083+ChristianPala@users.noreply.github.com>
2022-09-25 19:15:11 +0200

    Description imputation, sure seems there is a ton of junk in here...

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     pd.concat([df_09, df_10], axis=0).to_csv('../data/online_sales_dataset.csv', index=False)
     df = pd.read_csv('../data/online_sales_dataset.csv')
     # First look
+    print("First look at the data:")
     print(df.head())
     print(df.info())
     # check the columns types:
@@ -27,43 +28,42 @@ if __name__ == '__main__':
         print(col, df[col].unique().size)
     # check the number of missing values in each column:
     print(df.isnull().sum())
+    # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
     # convert InvoiceDate to datetime:
     df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'], format='%d/%m/%Y %H:%M')
 
     # check the dataset size:
-    print(df.shape)
+    size = df.shape[0]
 
-    # check for duplicates after the merge of the two csvs:
+    # check for duplicates after the merge of the two csv files:
     df = df.drop_duplicates(keep='first')
 
-    # check the dataset size:
-    print(df.shape)
+    # check if duplicates were removed:
+    print(f'Number of duplicates removed: {df.shape[0] - size}')
 
     # drop all missing values, for now while CB is working on imputing:
-    df.dropna(inplace=True)
+    # df.dropna(inplace=True)
 
     # print unique Customer ID's:
-    print(df['Customer ID'].unique().size)
+    print(f"We have: {df['Customer ID'].unique().size} unique customers")
 
     # find the maximum time elapsed between invoices from the same customer:
-    print(df.groupby('Customer ID')['InvoiceDate'].diff().max())
+    print(f"The longest repurchase in the dataset happened after: "
+          f"{df.groupby('Customer ID')['InvoiceDate'].diff().max()} ")
     # not very promising, we need to check how many "false positives" for a given time window there are then,
     # to decide on the churn threshold.
 
-    print((df.groupby('Customer ID')['InvoiceDate'].max() - df.groupby('Customer ID')['InvoiceDate'].min() >
-           pd.Timedelta(days=31)).sum())
+    thresholds = {31, 62, 93, 186, 365}
 
-    print((df.groupby('Customer ID')['InvoiceDate'].max() - df.groupby('Customer ID')['InvoiceDate'].min() >
-           pd.Timedelta(days=62)).sum())
+    for t in thresholds:
+        print(f"False positives after {t} days:"
+              f"{(df.groupby('Customer ID')['InvoiceDate'].max() - df.groupby('Customer ID')['InvoiceDate'].min() > pd.Timedelta(days=t)).sum()}")
 
-    print((df.groupby('Customer ID')['InvoiceDate'].max() - df.groupby('Customer ID')['InvoiceDate'].min() >
-           pd.Timedelta(days=93)).sum())
-
-    print((df.groupby('Customer ID')['InvoiceDate'].max() - df.groupby('Customer ID')['InvoiceDate'].min() >
-           pd.Timedelta(days=186)).sum())
-
-    print((df.groupby('Customer ID')['InvoiceDate'].max() - df.groupby('Customer ID')['InvoiceDate'].min() >
-           pd.Timedelta(days=365)).sum())
+    # replace missing description with the description of other invoices if StockCode is the same:
+    df.sort_values(by='StockCode', inplace=True)
+    df['Description'].fillna(method='ffill', inplace=True)
+    df.to_csv(Path('../data/online_sales_dataset_description_imputed.csv'), index=False)
 
 
 

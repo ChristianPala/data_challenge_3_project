8ad5226edf98780873bb076d89239c79f20347b2
Christian Berchtold <christian.berchtold@student.supsi>
2022-11-04 18:40:08 +0100

    eda colors

--- preprocessing/exploratory_data_analysis.py
+++ preprocessing/exploratory_data_analysis.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     # --------------------------------------------------------------
 
     # plot the correlation matrix, excluding the CustomerId column:
-    plt.figure(figsize=(10, 10))
+    # plt.figure(figsize=(10, 10))
     corr_matrix = df.drop('Customer ID', axis=1).corr(numeric_only=True)
     sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
     plt.title('Correlation matrix')
@@ -64,29 +64,29 @@ if __name__ == '__main__':
     plt.close()
 
     # plot the boxplot for the quantity column
-    plt.figure(figsize=(10, 10))
+    # plt.figure(figsize=(10, 10))
     sns.boxplot(x=df['Quantity'], showfliers=True, showmeans=True, meanline=True)
     plt.title('Boxplot for the quantity column')
     plt.savefig(Path('EDA', 'boxplot_quantity.png'))
     plt.close()
 
     # plot the boxplot for the price column
-    plt.figure(figsize=(10, 10))
+    # plt.figure(figsize=(10, 10))
     sns.boxplot(x=df['Price'], showfliers=True, showmeans=True, meanline=True)
     plt.title('Boxplot for the price column')
     plt.savefig(Path('EDA', 'boxplot_price.png'))
     plt.close()
 
     # distribution of the quantity column:
-    plt.figure(figsize=(10, 10))
-    sns.distplot(df['Quantity'], kde=False)
+    # plt.figure(figsize=(10, 10))
+    sns.distplot(df['Quantity'], kde=False, color='b')
     plt.title('Distribution of the quantity column')
     plt.savefig(Path('EDA', 'distribution_quantity.png'))
     plt.close()
 
     # distribution of the price column:
-    plt.figure(figsize=(10, 10))
-    sns.distplot(df['Price'], kde=False)
+    # plt.figure(figsize=(10, 10))
+    sns.distplot(df['Price'], kde=False, color='b')
     plt.title('Distribution of the price column')
     plt.savefig(Path('EDA', 'distribution_price.png'))
     plt.close()
@@ -99,8 +99,8 @@ if __name__ == '__main__':
     df['Country'] = df['Country'].astype('category')
 
     # plot the count plot for the country column:
-    plt.figure(figsize=(10, 10))
-    sns.countplot(x=df['Country'])
+    # plt.figure(figsize=(10, 10))
+    sns.countplot(x=df['Country'], color='b')
     plt.xticks(rotation=90)
     # give more space to the x_labels:
     plt.subplots_adjust(bottom=0.3)
@@ -109,8 +109,8 @@ if __name__ == '__main__':
     plt.close()
 
     # the dataset is dominated by UK customers, check the distribution without the UK:
-    plt.figure(figsize=(10, 10))
-    sns.countplot(x=df[df['Country'] != 'United Kingdom']['Country'])
+    # plt.figure(figsize=(10, 10))
+    sns.countplot(x=df[df['Country'] != 'United Kingdom']['Country'], color='b')
     plt.xticks(rotation=90)
     # give more space to the x_labels:
     plt.subplots_adjust(bottom=0.3)
@@ -125,32 +125,32 @@ if __name__ == '__main__':
     df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
 
     # plot the count plot for the invoice month column:
-    plt.figure(figsize=(10, 10))
-    sns.countplot(x=df['InvoiceDate'].dt.month)
+    # plt.figure(figsize=(10, 10))
+    sns.countplot(x=df['InvoiceDate'].dt.month, color='b')
     plt.title('Count plot for the invoice month column')
     plt.savefig(Path('EDA', 'count_plot_invoice_month.png'))
     plt.close()
     # we have more sales in november and december for this dataset, to consider when we decide the train/test split.
 
     # plot the count plot for the invoice day column:
-    plt.figure(figsize=(10, 10))
-    sns.countplot(x=df['InvoiceDate'].dt.day)
+    # plt.figure(figsize=(10, 10))
+    sns.countplot(x=df['InvoiceDate'].dt.day, color='b')
     plt.title('Count plot for the invoice day column')
     plt.savefig(Path('EDA', 'count_plot_invoice_day.png'))
     plt.close()
     # there is no clear pattern in the day of the month.
 
     # plot the count plot for the invoice day of week column:
-    plt.figure(figsize=(10, 10))
-    sns.countplot(x=df['InvoiceDate'].dt.dayofweek)
+    # plt.figure(figsize=(10, 10))
+    sns.countplot(x=df['InvoiceDate'].dt.dayofweek, color='b')
     plt.title('Count plot for the invoice day of week column')
     plt.savefig(Path('EDA', 'count_plot_invoice_day_of_week.png'))
     plt.close()
     # surprisingly for an online store, we have no sales on saturday, not sure how to interpret this.
 
     # plot the count plot for the invoice hour column:
-    plt.figure(figsize=(10, 10))
-    sns.countplot(x=df['InvoiceDate'].dt.hour)
+    # plt.figure(figsize=(10, 10))
+    sns.countplot(x=df['InvoiceDate'].dt.hour, color='b')
     plt.title('Count plot for the invoice hour column')
     plt.savefig(Path('EDA', 'count_plot_invoice_hour.png'))
     plt.close()
@@ -169,12 +169,13 @@ if __name__ == '__main__':
     # show the plots for number of purchases and total money spent
     x = df_agg.index
 
-    plt.figure(figsize=(10, 10))
-    plt.scatter(x=x, y=df_agg['NumberOfPurchases'], marker='.')
+    # plt.figure(figsize=(10, 10))
+    plt.scatter(x=x, y=df_agg['NumberOfPurchases'], marker='.', c='b')
     mean_n_purch = np.mean(df_agg['NumberOfPurchases']).astype(int)
     npurch = df_agg['NumberOfPurchases']
     y_average1 = npurch.rolling(window=mean_n_purch).mean()
     plt.plot(x, y_average1, label='Rolling mean', linestyle='-', c='orange')  # mean line
+    plt.xticks(rotation=90)
     plt.xlabel('datetime')
     plt.ylabel('number of purchases')
     plt.ylim(0, 800)
@@ -183,12 +184,13 @@ if __name__ == '__main__':
     plt.savefig(Path('EDA', 'nr_purchases_trend.png'))
     plt.close()
 
-    plt.figure(figsize=(10, 10))
-    plt.scatter(x, df_agg['TotalSpent'], marker='.')
+    # plt.figure(figsize=(10, 10))
+    plt.scatter(x, df_agg['TotalSpent'], marker='.', c='b')
     mean_tot_spend = np.mean(df_agg['TotalSpent']).astype(int)
     tot_spend = df_agg['TotalSpent']
     y_average2 = tot_spend.rolling(window=mean_tot_spend).mean()
     plt.plot(x, y_average2, label='Rolling mean', linestyle='-', c='orange')  # mean line
+    plt.xticks(rotation=90)
     plt.ylim(0, 10000)
     plt.xlabel('datetime')
     plt.ylabel('money spent')

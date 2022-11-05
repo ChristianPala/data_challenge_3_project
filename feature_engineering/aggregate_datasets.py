0c51eb6e72eba54287f7f5801842f31e0e09632f
Christian Berchtold <christian.berchtold@student.supsi>
2022-11-05 13:17:37 +0100

    renamed files to avoid confusion

--- feature_engineering/aggregate_datasets.py
+++ feature_engineering/aggregate_datasets.py
@@ -7,7 +7,7 @@ from pathlib import Path
 if __name__ == '__main__':
 
     # import the RFM dataset:
-    df = pd.read_csv(Path('', '..', 'data', 'online_sales_dataset_agg.csv'))
+    df = pd.read_csv(Path('..', 'data', 'online_sales_dataset_agg.csv'))
 
     y = df['CustomerChurned']
 
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 
     # add the features from the time series analysis:
     # import the time series dataset:
-    df_ts = pd.read_csv(Path('', '..', 'data', 'online_sales_dataset_tsfel_for_fs.csv'))
+    df_ts = pd.read_csv(Path('..', 'data', 'online_sales_dataset_tsfel_for_fs.csv'))
 
     # check how many customers are in the aggregated dataset and not in the time series dataset:
     print(f"Number of customers in the aggregated dataset and not in the time series dataset: "
@@ -30,12 +30,12 @@ if __name__ == '__main__':
     df = df.merge(df_ts, how='left', on='CustomerId')
 
     # import the nlp dataset:
-    df_nlp = pd.read_csv(Path('', '..', 'data', 'online_sales_dataset_nlp_for_fs.csv'), index_col=0)
+    df_nlp = pd.read_csv(Path('..', 'data', 'online_sales_dataset_nlp_for_fs.csv'), index_col=0)
     # merge the nlp dataset with the aggregated dataset:
     df = df.merge(df_nlp, how='left', left_on='CustomerId', right_on='CustomerId')
 
     # import the graph dataset:
-    df_graph = pd.read_csv(Path('', '..', 'data', 'online_sales_dataset_graph_for_fs.csv'), index_col=0)
+    df_graph = pd.read_csv(Path('..', 'data', 'online_sales_dataset_graph_for_fs.csv'), index_col=0)
     # merge the graph dataset with the aggregated dataset:
     df = df.merge(df_graph, how='left', left_on='CustomerId', right_on='CustomerId')
 
@@ -44,7 +44,7 @@ if __name__ == '__main__':
         raise Exception('There are missing values in the dataset.')
 
     # save the dataset, the first row is the header:
-    df.to_csv(Path('', '..', 'data', 'online_sales_dataset_for_fs.csv'), index=False)
+    df.to_csv(Path('..', 'data', 'online_sales_dataset_for_fs.csv'), index=False)
     # print the number of features:
     print(f"Number of features: {df.shape[1] -1}")
 

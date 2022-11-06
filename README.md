3439b448a574a7199d549802633f9447f8c6c05c
Christian Berchtold <christian.berchtold@student.supsi>
2022-11-06 12:11:27 +0100

    small bug fixes, comments

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Repository for the first data challenge 3 project, SUPSI DS &amp; AI course.
 
 ### Important notes
 We recommend to use this project with the python version 3.10.
-Please check that all the required libraries are installed (requirements.txt or at the end of this file). 
+
+Please check that all the required libraries are installed (chapter at the end of this file). 
 For the correct functioning of this project please copy the file located in the ./main/mod directory called "features.py"
 to the source files of the TSFEL library.
 The default TSFEL library path is: 
--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 # Libraries
-from preprocessing.pre_main import main as preprocessing
-from feature_engineering.fe_main import main as feature_engineering
-from feature_selection.fs_main import main as feature_selection
-from dimensionality_reduction.dr_main import main as dimensionality_reduction
-from error_analysis.error_analysis_with_shap_values import main as error_analysis
+from preprocessing.pre_main import main as prepro
+from feature_engineering.fe_main import main as fe
+from feature_selection.fs_main import main as fs
+from dimensionality_reduction.dr_main import main as dr
+from error_analysis.error_analysis_with_shap_values import main as ea
 from modelling.model_main import main as run_models
+import os
+from pathlib import Path
+
+
+def create_directories():
+    # data dir
+    data_path = Path('data')
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
+    params_path = Path('data', 'best_params')
+    if not os.path.exists(params_path):
+        os.makedirs(params_path)
 
 
 def main(run_error_analysis=False, run_model=False):
-    # running preprocessing (includes data loading and other)
-    preprocessing(preprocessing_only=True)
-    feature_engineering()
-    feature_selection(run_wrappers=False, run_correlation=False)
-    dimensionality_reduction(run_full=False, run_dsa=False)
+    create_directories()
+    prepro(preprocessing_only=True)
+    fe()
+    fs(run_wrappers=False, run_correlation=False)
+    dr(run_full=False, run_dsa=False)
 
     if run_model:
         run_models()
 
     if run_error_analysis:
-        error_analysis()
+        ea()
 
 
 if __name__ == '__main__':
